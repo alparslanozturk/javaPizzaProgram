253516d2e9b7b5a3d2f56e068d01430b7d347f46
Alparslan <alparslan.ozturk@gmail.com>
2019-05-22 14:57:28 +0300

    ilk commit

---
+++ src/Pizza.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Pizza {
+
+    public String tip;
+    public String boy;
+    public ArrayList<String> malzeme = new ArrayList<>();
+    public Boolean servis;
+    public Double tutar;
+
+    public Pizza() {
+        tip = "NORMAL";
+        boy = "KUCUK";
+        // malzeme.add("BIBER");
+        servis = false;
+    }
+
+    public void malzemeEkle(String m) {
+        malzeme.add(m);
+    }
+
+}
