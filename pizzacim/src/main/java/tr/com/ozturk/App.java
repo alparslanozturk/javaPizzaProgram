ff3cdd587b24e5d031798bf0a06ecb5b1d8beb92
aozturk <alparslan.ozturk@assistt.com.tr>
2020-02-05 16:57:37 +0300

    maven gecis

---
+++ pizzacim/src/main/java/tr/com/ozturk/App.java
@@ -0,0 +1,13 @@
+package tr.com.ozturk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
